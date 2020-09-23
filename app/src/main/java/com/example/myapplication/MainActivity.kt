8d35c606a65bc9a8e55de93da7d74d79c8b25c45
Michael Ellenwood <michael.ellenwood@csgi.com>
2020-09-23 11:45:05 -0500

    player plays mp4 video file

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun navigateToPlayerActivity(view : View) {
+        val intent = Intent(this, PlayerActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/myapplication/PlayerActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_player.*
+
+class PlayerActivity : AppCompatActivity() {
+
+    private var exoPlayer : SimpleExoPlayer? = null
+
+    companion object {
+        private const val DISNEY_MEDIA_SOURCE_URI = "https://html5demos.com/assets/dizzy.mp4"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+        player_view.player = exoPlayer
+
+        val dataSource = DefaultDataSourceFactory(this, Util.getUserAgent(this, "exo-demo"))
+        val mediaSource = ExtractorMediaSource.Factory(dataSource).createMediaSource(Uri.parse(
+            DISNEY_MEDIA_SOURCE_URI))
+
+        exoPlayer?.prepare(mediaSource)
+        exoPlayer?.playWhenReady = true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player_view.player = null
+        exoPlayer?.release()
+        exoPlayer = null
+    }
+}
